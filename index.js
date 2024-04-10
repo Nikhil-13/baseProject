5088da4fb26100a5ee8cb13da0671cd8786f16bc
Nikhil-13 <nnik270@gmail.com>
2024-04-10 18:24:46 +0530

    removed reactotron

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
-if (__DEV__) {
-  import('./reactotron.config').then(() =>
-    console.log('[REACTOTRON] - Configured!'),
-  );
-}
 
 AppRegistry.registerComponent(appName, () => App);
--- src/screens/authentication/welcomeScreen/index.js
+++ src/screens/authentication/welcomeScreen/index.js
@@ -1,8 +1,6 @@
-import {Text, View} from 'react-native';
 import React, {Component} from 'react';
 import {ScreenContainer} from '../../../components/common';
 import StyledText from '../../../components/common/styledText';
-import {thunk} from 'redux-thunk';
 import Config from 'react-native-config';
 
 export class WelcomeScreen extends Component {
