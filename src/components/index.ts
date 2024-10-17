a5250c6dc0b7dd2d47292422a635015fa4862541
Nikhil-13 <nnik270@gmail.com>
2024-10-17 22:33:06 +0530

    changes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export {default as ScreenContainer} from './common/screenConatiner';
 export {default as CustomTabBar} from './common/customTabBar';
+export {default as StyledText} from './common/styledText';
